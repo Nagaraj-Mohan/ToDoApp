21cd69408daa52a36fea96d97f446ad4c6163bda
nagaraj <nagarajmohan.mba@gmail.com>
2024-09-15 22:45:56 +0530

    updated main for the project with case read, add and exit

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 #Python To DO APP
-print("-- To Do APP --")
 
-print("What you want to do (add, read, edit, complete) in to do app?:")
-user_input = input()
+print("-- To Do APP Manager --")
 
-match user_input:
-    case add:
+while True:
+    print("What you want to do (add, read, edit, complete, exit) in to do app?:")
+    user_input = input()
+    match user_input:
+        case "add":
+
+            print("Enter the task you want to add:")
+            task_item = input()
+
+            with open("files/todo.txt", "r") as file:
+                todos = file.readlines()
+
+            todos.append(task_item + "\n")
+
+            with open("files/todo.txt", "w") as file:
+                file.writelines(todos)
+
+            print(f"Added {task_item} to the todo list")
+
+        case "read":
+            with open("files/todo.txt", "r") as file:
+                todos = file.readlines()
+
+            for index, item in enumerate(todos):
+                print(f"{index+1} - {item.strip()}")
+
+        case _:
+            print("Bye")
+            break
+
