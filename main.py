53b378e78c990229ae8161ef003bbacc95e317f1
nagaraj <trianglejet@gmail.com>
2024-09-23 23:49:15 +0530

    Added if condition Added elif and else

--- main.py
+++ main.py
@@ -5,63 +5,60 @@ print("-- To Do APP Manager --")
 while True:
     print("What you want to do (add, read, edit, complete, exit) in to do app?:")
     user_input = input()
-    match user_input:
-        case "add":
+    if 'add' in user_input:
 
-            print("Enter the task you want to add:")
-            task_item = input()
+        task_item = user_input[4:]
 
-            with open("files/todo.txt", "r") as file:
-                todos = file.readlines()
+        with open("files/todo.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(task_item + "\n")
+        todos.append(task_item + "\n")
 
-            with open("files/todo.txt", "w") as file:
-                file.writelines(todos)
+        with open("files/todo.txt", "w") as file:
+            file.writelines(todos)
 
-            print(f"Added {task_item} to the todo list")
+        print(f"Added {task_item} to the todo list")
 
-        case "read":
-            with open("files/todo.txt", "r") as file:
-                todos = file.readlines()
+    elif "read" in user_input:
+        with open("files/todo.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                print(f"{index+1} - {item.strip()}")
+        for index, item in enumerate(todos):
+            print(f"{index+1} - {item.strip()}")
 
-        case "edit":
-            with open("files/todo.txt", "r") as file:
-                todos = file.readlines()
+    elif "edit" in user_input:
+        with open("files/todo.txt", "r") as file:
+            todos = file.readlines()
 
-            print("Which item no. you want to edit?")
-            item_no = int(input())
+        item_no = int(user_input[5:])
 
-            print("What is the new task for this item?")
-            item_value = input()+"\n"
+        print("What is the new task for this item?")
+        item_value = input()+"\n"
 
-            todos[item_no-1] =item_value
-            with open("files/todo.txt", "w") as file:
-                file.writelines(todos)
+        todos[item_no-1] =item_value
+        with open("files/todo.txt", "w") as file:
+            file.writelines(todos)
 
-            print(f"Updated {item_value.strip("\n")} to the todo list")
+        print(f"Updated {item_value.strip("\n")} to the todo list")
 
-        case "complete":
-            with open("files/todo.txt", "r") as file:
-                todos = file.readlines()
+    elif "complete" in user_input:
+        with open("files/todo.txt", "r") as file:
+            todos = file.readlines()
 
-            print("Which item no. you have completed?")
-            item_no = int(input())
+        print("Which item no. you have completed?")
+        item_no = int(user_input[9:])
 
-            to_remove_item = todos[item_no-1]
-            todos.pop(item_no-1)
+        to_remove_item = todos[item_no-1]
+        todos.pop(item_no-1)
 
-            with open("files/todo.txt", "w") as file:
-                file.writelines(todos)
+        with open("files/todo.txt", "w") as file:
+            file.writelines(todos)
 
-            print(f"Removed {to_remove_item.strip("\n")} from the todo list")
+        print(f"Removed {to_remove_item.strip("\n")} from the todo list")
 
-        case "exit":
-            print("Bye")
-            break
+    elif "exit" in user_input:
+        print("Bye")
+        break
 
-        case _ :
-            print("This command is not recognized")
+    else:
+        print("This command is not recognized")
